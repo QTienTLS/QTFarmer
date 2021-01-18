84fcbcb9436d2e097b400164cb80d0a51e3ff697
unknown <quangtientls@gmail.com>
2021-01-19 06:56:13 +0700

    commit

--- src/routes/site.route.js
+++ src/routes/site.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const route = express.Router();
 const 
-siteController = require('../app/controllers/SiteControllers');
+            siteController = require('../app/controllers/SiteControllers');
 
 route.use('/', siteController.home);
 
