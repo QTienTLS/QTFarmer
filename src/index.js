ea8ff9526afca8146c0b5964dbe5dc3e51f31e40
unknown <quangtientls@gmail.com>
2021-01-19 07:01:35 +0700

    commit1

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const port = 3001;
 const route = require('./routes');
 
 app.use(express.static('src\\public'));
-app.engine('hbs',
+app.engine(
+    'hbs',
     handlebars({
         extname: '.hbs',
     }),
 );
-app.set(
-  'view engine', 'hbs');
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
 app.use(morgan('combined'));
