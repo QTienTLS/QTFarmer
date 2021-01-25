777c8c5a743f92bcea1139a9839df234459cd44a
QTienTLS <55350592+QTienTLS@users.noreply.github.com>
2021-01-25 08:48:50 +0700

    m

--- src/routes/seed.route.js
+++ src/routes/seed.route.js
@@ -3,6 +3,7 @@ const route = express.Router();
 const seedController = require('../app/controllers/SeedControllers');
 
 route.get('/all', seedController.all);
+route.get('/:id/detail', seedController.all);
 route.get('/:slug', seedController.show);
 
 module.exports = route;
